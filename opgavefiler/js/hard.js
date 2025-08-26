bc8041635bef6b760b108a2d3726c781a6523355
MalteEngelund <malte.engelund@hotmail.com>
2025-08-26 15:02:01 +0200

    Update hard.js
    "hard" Opgave 1

--- opgavefiler/js/hard.js
+++ opgavefiler/js/hard.js
@@ -3,6 +3,22 @@
 
 
 console.group("opgave 1");
+const todoApp = {
+gardner: {
+name: "Have",
+items: [
+{ text: "Slå græs", done: true },
+{ text: "Klip hæk", done: true }
+]
+},
+seng: {
+name: "sov",
+items: [
+{ text: "sov", done: false },
+{ text: "sov igen", done: false }]}
+};
+console.log(todoApp);
+//hint brugt
 
 console.groupEnd();
 /* opgave 2 */

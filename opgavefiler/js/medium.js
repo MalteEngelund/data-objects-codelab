fc2b8c806271840b519af8ba308c4e9b3aab1f17
MalteEngelund <malte.engelund@hotmail.com>
2025-08-26 14:34:53 +0200

    Update medium.js
    "medium" opgaver

--- opgavefiler/js/medium.js
+++ opgavefiler/js/medium.js
@@ -1,5 +1,10 @@
 /* Opgave 1*/
 console.group("opgave 1");
+const liste = {
+  items: ["test1", "test2", "test3", "test4", "test5"]
+}
+liste.items.push('test6')
+console.log(liste);
 
 
 
@@ -9,6 +14,8 @@ console.groupEnd();
 
 /*opgave 2*/
 console.group("opgave 2");
+liste.items.splice(1, 1)
+console.log(liste);
 
 
 
@@ -17,6 +24,10 @@ console.groupEnd();
 
 /*opgave 3*/
 console.group("opgave 3");
+liste.items = liste.items.map(item =>
+  item === "test3" ? "changed" : item
+);
+console.log(liste.items);
 
 console.groupEnd();
 

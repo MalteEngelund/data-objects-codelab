ad91048270dc8db30e747d3f9a244cc701cb95cd
MalteEngelund <malte.engelund@hotmail.com>
2025-08-26 11:48:29 +0200

    Update easy.js
    "easy" opgaver

--- opgavefiler/js/easy.js
+++ opgavefiler/js/easy.js
@@ -1,18 +1,26 @@
 /* opgave 1 */
 console.group("opgave 1");
-
+const person = {
+  name: "Bob",
+  age: "40",
+  job: "Builder",
+};
+console.log(person);
 console.groupEnd();
 
 /* opgave 2 */
 console.group("opgave 2");
-
+console.log(person.name);
+console.log(person.age);
+console.log(person.job);
 
 
 console.groupEnd();
 
 /* opgave 3 */
 console.group("opgave 3");
-
+delete person.age
+console.log(person);
 
 
 console.groupEnd();
